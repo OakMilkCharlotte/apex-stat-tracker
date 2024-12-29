94d8e5e3b23eebd55561f6e6b0c379d6fe124ed7
OakMilkCharlotte <matt.whetten@gmail.com>
2024-12-29 15:50:08 -0700

    installed npm web vitals

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. How about I edit your mom's bank account =OwO=
+          Edit <code>src/App.js</code> and save to reload. How about I edit your mom's bank account fatty =OwO=
         </p>
         <a
           className="App-link"
